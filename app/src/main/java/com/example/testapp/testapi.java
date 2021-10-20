7c5c68dc8e8c6bc68e3ed14220a53a5a788082ef
arinchai <jjza139@hotmail.com>
2021-10-20 21:39:20 +0700

    Test api Authentication +Token +RefreshToken

--- app/src/main/java/com/example/testapp/testapi.java
+++ app/src/main/java/com/example/testapp/testapi.java
@@ -11,37 +11,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.Objects;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.Headers;
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class testapi extends AppCompatActivity {
-    private Button sendreq;
-    private TextView result;
+    private Button sendreq ,btn_refresh;
+    private TextView result,edit_Token,edit_refresh;
     private OkHttpClient client = new OkHttpClient();
+    String token ="" ;
+    String refreshtoken="";
+    String b ="";
     String a = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testapi);
-//        Intent intent = getIntent();
-//        String data = intent.getDataString();
-//        String code[] = data.split("=");
+
+
+      // String code[] = data.split("=");
         result = findViewById(R.id.result);
+        edit_Token = findViewById(R.id.edit_Token);
+        edit_Token.setText("Token : "+token);
+        edit_refresh = findViewById(R.id.edit_refresh);
+        edit_refresh.setText("Token Refresh : "+refreshtoken);
         sendreq = findViewById(R.id.sendreq);
         sendreq.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +51,38 @@ public class testapi extends AppCompatActivity {
                 get_auth();
             }
         });
+        btn_refresh = findViewById(R.id.btn_refreshtoken);
+        btn_refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                post_refreshtoken(refreshtoken);
+            }
+        });
 
+    }
+    private void updata_data(){
+        edit_Token.setText("Token : "+token);
+        edit_refresh.setText("Token Refresh: "+refreshtoken);
+    }
+
+    private void update_code(){
+        Intent intent = getIntent();
+        try {
+            String code[] = intent.getDataString().split("=");
+            if(code[1].length() > 30){
+                result.setText("Authcode: "+code[1]);
+                post_token(code[1]);
+            }
+
+        }catch (Exception e){
+            // result.setText(data);
+
+        }
+    }
+
+    protected void onStart(){
+        super.onStart();
+        update_code();
     }
 
     private void post_auth() {
@@ -125,6 +158,103 @@ public class testapi extends AppCompatActivity {
         });
 
     }
+
+    private void post_token(String authcode) {
+        //[code request api ]
+        // [Code create json and body]
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("applicationKey", "l7b26b44c713e746dfa96fca5e635ca566");
+            jsonObject.put("applicationSecret", "9f1c5b1884aa4e0285a214ac39ce48ef");
+            jsonObject.put("authCode", authcode);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token")
+                .post(body)
+                .header("Content-Type", "application/json")
+                .addHeader("resourceOwnerId", "l7b26b44c713e746dfa96fca5e635ca566")
+                .addHeader("requestUId", "{{$guid}}")
+                .addHeader("accept-language", "EN")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) { }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                a=response.body().string();
+                try {
+                    JSONObject json = new JSONObject(a);
+                    JSONObject data = new JSONObject(json.getString("data"));
+                    token=data.getString("accessToken");
+                    refreshtoken=data.getString("refreshToken");
+                    updata_data();
+//                    result.setText(link.getString("accessToken"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                result.setText(a);
+
+
+            }
+        });
+
+    }
+
+    private void post_refreshtoken(String Refreshtoken) {
+        //[code request api ]
+        // [Code create json and body]
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("applicationKey", "l7b26b44c713e746dfa96fca5e635ca566");
+            jsonObject.put("applicationSecret", "9f1c5b1884aa4e0285a214ac39ce48ef");
+            jsonObject.put("refreshToken", Refreshtoken);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token/refresh")
+                .post(body)
+                .header("Content-Type", "application/json")
+                .addHeader("resourceOwnerId", "l7b26b44c713e746dfa96fca5e635ca566")
+                .addHeader("requestUId", "{{$guid}}")
+                .addHeader("accept-language", "EN")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) { }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                a=response.body().string();
+                try {
+                    JSONObject json = new JSONObject(a);
+                    JSONObject data = new JSONObject(json.getString("data"));
+                    token=data.getString("accessToken");
+                    refreshtoken=data.getString("refreshToken");
+                    updata_data();
+//                    result.setText(link.getString("accessToken"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                result.setText(a);
+
+
+            }
+        });
+
+    }
+
 
     private void go2scb(String Link) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Link));
